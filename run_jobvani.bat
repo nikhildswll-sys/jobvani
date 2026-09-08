c08f8182a34a44cd26ccc34c7ea4af85fd86ae66
nikhildswll-sys <nikhil@jobvani.in>
2026-09-08 13:08:53 +0530

    delete all files

--- run_jobvani.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title JobVani - Sarkari Naukri Portal Server
-cd /d "%~dp0"
-echo ===================================================
-echo Starting JobVani Server on http://127.0.0.1:8000 ...
-echo ===================================================
-python start_server.py
-pause
